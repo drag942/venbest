a63f00bbfb2db6972cbb1a5fdbe3b868aae5e6a6
Dima <infermo94.09@gmail.com>
2020-10-20 13:36:26 +0300

    Updates

---
+++ precache-manifest.bf54924876f076a369eeb33f03e0fdd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb968c644ccde30d86f1e366a3651e20",
+    "url": "/venbest/index.html"
+  },
+  {
+    "revision": "937049c6816664eb53ac",
+    "url": "/venbest/static/css/main.b2842323.chunk.css"
+  },
+  {
+    "revision": "a979c35691e1390d1fe9",
+    "url": "/venbest/static/js/2.05ba1d67.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/venbest/static/js/2.05ba1d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937049c6816664eb53ac",
+    "url": "/venbest/static/js/main.17597485.chunk.js"
+  },
+  {
+    "revision": "93b92b35fd5896c98c29",
+    "url": "/venbest/static/js/runtime-main.2386618a.js"
+  }
+]);
